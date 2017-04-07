adc7b8abd7df95f0a7ea3f7eb2e79b07426ac450
Unknown <antoguida5@gmail.com>
2017-04-07 17:52:33 -0300

    manzana

---
+++ HospitalParense/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HospitalParense.Global" Language="C#" %>
---
+++ HospitalParense/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HospitalParense.Startup))]
+namespace HospitalParense
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
